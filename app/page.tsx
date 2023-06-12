e45b8974a818cc749acedb1223a454023a01832a
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2023-06-13 09:52:22 +1000

    Add sectionContainer

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,12 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import { Text } from "@chakra-ui/react";
+import SectionContainer from "./components/SectionContainer";
 
 export default function Home() {
-  return <Text textStyle={"context"}>This is a heading</Text>;
+  return (
+    <SectionContainer>
+      <Text textStyle={"context"}>This is a heading</Text>
+    </SectionContainer>
+  );
 }
