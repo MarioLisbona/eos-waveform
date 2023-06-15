314423e43f8a32f9fd69246fa3a2b8e47fa02365
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2023-06-15 16:13:53 +1000

    Revert "Wrap each clip object grid in form control"
    This reverts commit 2a9d0708def1cbc430ffd06f2fbd1653abd26385.

--- app/components/waveform/components/ClipGrid.tsx
+++ app/components/waveform/components/ClipGrid.tsx
@@ -9,7 +9,6 @@ import {
   Flex,
   Text,
   Box,
-  FormControl,
 } from "@chakra-ui/react";
 import format from "format-duration";
 import { useState } from "react";
@@ -24,62 +23,49 @@ export default function ClipGrid({
   myPeaks: PeaksInstance | undefined;
   setSegments: React.Dispatch<React.SetStateAction<TestSegmentProps[]>>;
 }) {
+  console.log("in ClipGrid", segments);
   return (
     <>
       {segments.length > 0 &&
-        segments.map((seg, idx) => {
-          const isError = seg.id == "";
-          console.log(isError);
-          return (
-            <FormControl key={idx}>
-              <Grid
-                templateColumns="repeat(8, 1fr)"
-                gap={6}
-                w={"100%"}
-                key={idx}
-                mb={"1rem"}
-              >
-                <GridItem colStart={1} colEnd={3}>
-                  <Input value={seg.id}></Input>
-                </GridItem>
-                <GridItem colStart={3} colEnd={5}>
-                  <Input
-                    value={format(seg.startTime * 1000, {
-                      leading: true,
-                      ms: true,
-                    })}
-                  ></Input>
-                </GridItem>
-                <GridItem colStart={5} colEnd={7}>
-                  <Input
-                    value={format(seg.endTime * 1000, {
-                      leading: true,
-                      ms: true,
-                    })}
-                  ></Input>
-                </GridItem>
-                <GridItem colStart={7} colEnd={9}>
-                  <Flex w={"100%"} justify={"flex-end"}>
-                    <Button variant={"waveformOutlined"}>Create</Button>
-                    <Button
-                      variant={"waveformOutlined"}
-                      onClick={() =>
-                        deleteSingleSegment(
-                          myPeaks,
-                          seg.id,
-                          segments,
-                          setSegments
-                        )
-                      }
-                    >
-                      Delete
-                    </Button>
-                  </Flex>
-                </GridItem>
-              </Grid>
-            </FormControl>
-          );
-        })}
+        segments.map((seg, idx) => (
+          <Grid
+            templateColumns="repeat(8, 1fr)"
+            gap={6}
+            w={"100%"}
+            key={idx}
+            mb={"1rem"}
+          >
+            <GridItem colStart={1} colEnd={3}>
+              <Input value={seg.id}></Input>
+            </GridItem>
+            <GridItem colStart={3} colEnd={5}>
+              <Input
+                value={format(seg.startTime * 1000, {
+                  leading: true,
+                  ms: true,
+                })}
+              ></Input>
+            </GridItem>
+            <GridItem colStart={5} colEnd={7}>
+              <Input
+                value={format(seg.endTime * 1000, { leading: true, ms: true })}
+              ></Input>
+            </GridItem>
+            <GridItem colStart={7} colEnd={9}>
+              <Flex w={"100%"} justify={"flex-end"}>
+                <Button variant={"waveformOutlined"}>Create</Button>
+                <Button
+                  variant={"waveformOutlined"}
+                  onClick={() =>
+                    deleteSingleSegment(myPeaks, seg.id, segments, setSegments)
+                  }
+                >
+                  Delete
+                </Button>
+              </Flex>
+            </GridItem>
+          </Grid>
+        ))}
     </>
   );
 }
