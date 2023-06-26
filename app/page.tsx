2cea1b63668cc4d269f52d97ca923d1cbaf5b636
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2023-06-26 16:12:12 +1000

    color change for created segment

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,13 @@
 "use client";
-import Image from "next/image";
-import styles from "./page.module.css";
 import { Flex, Text } from "@chakra-ui/react";
 import SectionContainer from "./components/SectionContainer";
 import WaveForm from "./components/waveform";
-import { AudioDataProps } from "./types";
 
 export default function Home() {
   return (
     <SectionContainer>
       <Flex direction={"column"} align={"center"}>
-        <Text textStyle={"subheading"}>EOS Waveform Prototype</Text>
+        <Text textStyle={"context"}>EOS Waveform Prototype</Text>
         <WaveForm />
       </Flex>
     </SectionContainer>
